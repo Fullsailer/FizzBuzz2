b6f1831004ba9f6e63aafe30e24a0499b220ecb9
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-08-07 16:47:54 -0400

    Finished site.js updated css,index, and app
    Final site.js code, updated CSS colors, updated index logo from image to video, added to code page, need to finish function descriptions.

--- js/site.js
+++ js/site.js
@@ -16,7 +16,7 @@ function getValues() {
     if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
 
         //call fizzBuzz
-        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        let fbArray = fizzBuzzC(fizzValue, buzzValue);
         //we call displayData
         displayData(fbArray);
     } else {
@@ -51,6 +51,53 @@ function fizzBuzz(fizzValue, buzzValue) {
     }
     return returnArray;
 }
+// Fizz Buzz with a swtich statment
+function fizzBuzzB(fizzValue, buzzValue) {
+
+    let returnArray = [];
+    let Fizz = false;
+    let Buzz = false;
+
+    //switch case selects which block to execute
+    for (let i = 1; i < 100; i++) {
+
+        Fizz = i % fizzValue == 0;
+        Buzz = i % buzzValue == 0;
+
+        switch (true) {
+            case Fizz && Buzz: {
+                returnArray.push(`FizzBuzz`);
+                break;
+            }
+            case Fizz: {
+                returnArray.push(`Fizz`);
+                break;
+            }
+            case Buzz: {
+                returnArray.push(`Buzz`);
+                break;
+            }
+            default: {
+                returnArray.push(i);
+                break;
+            }
+        }
+    }
+
+    return returnArray;
+}
+// Fizz Buzz with a ternary operator
+function fizzBuzzC(fizzValue, buzzValue) {
+    let returnArray = [];
+
+    //Ternary Operator or three operands or short cut for if statement.
+    for (let i = 1; i <= 100; i++) {
+        let value = ((i % fizzValue == 0 ? `Fizz` : '') + (i % buzzValue == 0 ? `Buzz` : '') || i);
+        returnArray.push(value);
+    }
+
+    return returnArray;
+}
 
 
 
@@ -74,17 +121,17 @@ function displayData(fbArray) {
 
         rowCols[0].classList.add(fbArray[index]);
         rowCols[0].textContent = fbArray[index];
-        
-        rowCols[1].classList.add(fbArray[index]);
+
+        rowCols[1].classList.add(fbArray[index + 1]);
         rowCols[1].textContent = fbArray[index + 1];
-        
-        rowCols[2].classList.add(fbArray[index]);
+
+        rowCols[2].classList.add(fbArray[index + 2]);
         rowCols[2].textContent = fbArray[index + 2];
-        
-        rowCols[3].classList.add(fbArray[index]);
+
+        rowCols[3].classList.add(fbArray[index + 3]);
         rowCols[3].textContent = fbArray[index + 3];
-        
-        rowCols[4].classList.add(fbArray[index]);
+
+        rowCols[4].classList.add(fbArray[index + 4]);
         rowCols[4].textContent = fbArray[index + 4];
 
         tableBody.appendChild(tableRow);
